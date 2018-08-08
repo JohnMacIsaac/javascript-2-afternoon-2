a04204f787ac897bd1c4d5ffaf0b8f48cfab5bde
John MacIsaac <john@Johns-MacBook-Pro.local>
2018-08-08 00:30:18 -0500

    completed

--- practice.js
+++ practice.js
@@ -5,7 +5,6 @@
 
   You can refresh the page at any time to re-run all the tests.
 */
-
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
@@ -99,37 +98,61 @@ function evenFinder(nums) {
 
 
 
-/////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
-////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
+///////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
+//////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
-// ////////// PROBLEM 6 //////////
-// // Do not edit the code below.
-// var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
-// // Do not edit the code above.
-// /*
-//   Write a function called divider that is given one argument, numbersArray.
-//   Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
-// */
-// //Code Here
-
-
-
-////////// PROBLEM 7 //////////
+////////// PROBLEM 6 //////////
 // Do not edit the code below.
-// var getRandomArbitrary = function() {
-//   return Math.floor(Math.random() * 30);
-// };
-// // Do not edit the code above.
+var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+// Do not edit the code above.
+/*
+  Write a function called divider that is given one argument, numbersArray.
+  Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
+*/
+//Code Here
+function divider (numbersArray) {
+  var oddsArr = [];
+  var evensArr = [];
+  for (var i = 0; i < numbersArray.length; i++) {
+    if (numbersArray[i] % 2 === 0) {
+      evensArr.push(numbersArray[i]);
+    } else {
+      oddsArr.push(numbersArray[i]);
+    }
+  }  
+  return [evensArr, oddsArr]; 
+}
+divider(numbersArray);
 
-// /* 
-//   var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-//   Above you're given a function that will return a random number between 0 and 30. There is also a commented out array full of numbers to help you visualize what your function will be receiving.
-//   Write a function named finder that will take in an array as an argument.
-//   It will then  get a random number (by invoking getRandomArbitrary).
-//   Loop through the array to see if that random number is in the array. 
-//   If it is, return true, if it's not, return false
-// */
-// //Code Here
+
+
+//////// PROBLEM 7 //////////
+// Do not edit the code below.
+var getRandomArbitrary = function() {
+  return Math.floor(Math.random() * 30);
+};
+// Do not edit the code above.
+
+/* 
+  var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+  Above you're given a function that will return a random number between 0 and 30. 
+  There is also a commented out array full of numbers to help you visualize what your function will be receiving.
+  Write a function named finder that will take in an array as an argument.
+  It will then  get a random number (by invoking getRandomArbitrary).
+  Loop through the array to see if that random number is in the array. 
+  If it is, return true, if it's not, return false
+*/
+//Code Here
+function finder(array) {
+  var randomNumber = getRandomArbitrary();
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === randomNumber) { 
+      return true;
+    }
+  }
+  return false;
+}
+
 
 
 
@@ -143,7 +166,6 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
   Write a function called removeItem that is given two arguments, the first is myGroceryList, and the second is an item to remove from myGroceryList. 
   If the second argument (or the item to add or remove) matches an item in myGroceryList, remove that item from the your grocery list and return the new, updated grocery list.
-
 
   Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList and the second is an item to add to your grocery list. 
   In addItem add the item you passed in to myGroceryList then return the new, updated grocery list.
@@ -159,6 +181,16 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
+function removeItem(myGroceryList, item) {
+  if(!item) return [];
+  return myGroceryList.filter(elem => elem !== item);
+}
+
+function addItem(myGroceryList, item) {
+  if (!myGroceryList || !item) return []; 
+  myGroceryList.push(item);
+  return myGroceryList;
+}
 
 
 // ////////// PROBLEM 9 //////////
@@ -168,7 +200,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 // */
 
 // //Code Here
-
+function maker() {
+  var newArr = [];
+  for (var i = 1; i <= 215; i++) {
+    newArr.push(i);
+  }
+  return newArr;
+}
 
 
 // ////////// PROBLEM 10 //////////
@@ -184,32 +222,34 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 // */
   
 // //Code Here
-
+function addTen(numbers) {
+  return numbers.map(elem => Number(elem) + 10)
+}
 
 
 // ////////// PROBLEM 11 //////////
 
-// // Do not edit the code below.
-// var num1 = Math.floor(Math.random() * 30);
-// var num2 = Math.floor(Math.random() * 30);
-// var arr1 = [];
-// var arr2 = [];
-// for(var i = 0; i < num1; i++){
-//   arr1.push(i);
-// }
-// for(var i = 0; i < num2; i++){
-//   arr2.push(i);
-// }
-// // Do not edit the code above.
-
-// /*
-//   Above is some code that adds a random number of values to both arr1 and arr2.
-//   Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. 
-//   Return the array which is longest.
-// */
-
-// //Code Here
-
+// Do not edit the code below.
+var num1 = Math.floor(Math.random() * 30);
+var num2 = Math.floor(Math.random() * 30);
+var arr1 = [];
+var arr2 = [];
+for(var i = 0; i < num1; i++){
+  arr1.push(i);
+}
+for(var i = 0; i < num2; i++){
+  arr2.push(i);
+}
+// Do not edit the code above.
+/*
+  Above is some code that adds a random number of values to both arr1 and arr2.
+  Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. 
+  Return the array which is longest.
+*/
+//Code Here
+function longer(arr1, arr2) {
+  return arr1.length > arr2.length ? arr1 : arr2;
+}
 
 
 // /*
@@ -221,100 +261,122 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 // */
 
 // //Code Here
-
+function both(arr1, arr2) {
+  var newArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
+  }
+  return newArr;
+}
 
 
 // ////////// PROBLEM 12 //////////
-// // Do not edit the code below.
-// var devMountainEmployees = [];
+// Do not edit the code below.
+var devMountainEmployees = [];
 
-// var tyler = {
-//     name: 'Tyler',
-//     position: 'Lead Instructor/Engineer',
-//     spiritAnimal: 'Honey Badger'
-// };
+var tyler = {
+    name: 'Tyler',
+    position: 'Lead Instructor/Engineer',
+    spiritAnimal: 'Honey Badger'
+};
 
-// var cahlan = {
-//     name: 'Cahlan',
-//     position: 'CEO',
-//     spiritAnimal: 'butterfly'
-// };
+var cahlan = {
+    name: 'Cahlan',
+    position: 'CEO',
+    spiritAnimal: 'butterfly'
+};
 
-// var ryan = {
-//     name: 'Ryan',
-//     position: 'Marketing',
-//     spiritAnimal: 'fox'
-// };
+var ryan = {
+    name: 'Ryan',
+    position: 'Marketing',
+    spiritAnimal: 'fox'
+};
 
-// var colt = {
-//     name: 'Colt',
-//     position: 'Everything really',
-//     spiritAnimal: 'Young Male Horse'
-// };
-// // Do not edit the code above.
+var colt = {
+    name: 'Colt',
+    position: 'Everything really',
+    spiritAnimal: 'Young Male Horse'
+};
+// Do not edit the code above.
 
-// /*
-//   Above you're given an empty array and four variables containing objects. 
-//   Fill the devMountainEmployees array with those four objects. 
-//   After that console.log the length of the Array and make sure that it's equal to 4. 
-// */
+/*
+  Above you're given an empty array and four variables containing objects. 
+  Fill the devMountainEmployees array with those four objects. 
+  After that console.log the length of the Array and make sure that it's equal to 4. 
+*/
 
-// //Code Here
+//Code Here
+// devMountainEmployees.push(tyler, cahlan, ryan, colt)
+devMountainEmployees = [tyler, cahlan, ryan, colt];
+// console.log(devMountainEmployees.length);
 
+/*
+  Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
+  Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
+*/
 
-
-// /*
-//   Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
-//   Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
-// */
-
-// //Code Here
-
-
-
-// ////////// PROBLEM 13 //////////
-// /*
-//   A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects) of Data is to have an Array full of objects. 
-//   Create an empty array called users.
-// */
-
-// //Code Here
+//Code Here
+devMountainEmployees = devMountainEmployees.filter(elem => elem.name !== "Cahlan");
 
 
 
-// /*
-//   Now add three user objects to your users array. Each user object should contain the following properties. name, email, password, username.
+////////// PROBLEM 13 //////////
+/*
+  A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects) of Data is to have an Array full of objects. 
+  Create an empty array called users.
+*/
+//Code Here
+var users = [];
 
-//   Include the following user1 object as one of the objects in your array.
-// */
+/*
+  Now add three user objects to your users array. Each user object should contain the following properties. name, email, password, username.
+  Include the following user1 object as one of the objects in your array.
+*/
+// Do not edit the code below.
+var user1 = {
+    name: 'Tyler McGinnis',
+    email: 'tylermcginnis33@gmail.com',
+    password: 'iLoveJavaScript',
+    username: 'infiniteLoop'
+};
+// Do not edit the code above.
+//Code Here
+users = [ 
+  { 
+    name: 'name',
+    email: "email",
+    password: "password",
+    username: "username",
+  },
+  {
+    name: 'name',
+    email: "email",
+    password: "password",
+    username: "username",
+  },
+  {
+    name: 'name',
+    email: "email",
+    password: "password",
+    username: "username",
+  },
+  user1
+ ];
 
-// // Do not edit the code below.
-// var user1 = {
-//     name: 'Tyler McGinnis',
-//     email: 'tylermcginnis33@gmail.com',
-//     password: 'iLoveJavaScript',
-//     username: 'infiniteLoop'
-// };
-// // Do not edit the code above.
+/*
+  Now you have a very common data structure. 
+  Twitter is a good use case.
+  It's easy to imagine that your followers list on Twitter is an Array full of objects and those objects contain properties about the specific person you follow.
 
-// //Code Here
+  Now let's say that Tyler decided to delete his account.
+  Loop through your array of objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
+  Once you find the particular index he's located in, delete him from the array.
+*/
+// Code Here
+users = users.filter(elem => elem.email !== "tylermcginnis33@gmail.com");
 
-
-
-// /*
-//   Now you have a very common data structure. 
-//   Twitter is a good use case.
-//   It's easy to imagine that your followers list on Twitter is an Array full of objects and those objects contain properties about the specific person you follow.
-
-//   Now let's say that Tyler decided to delete his account.
-//   Loop through your array of objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
-//   Once you find the particular index he's located in, delete him from the array.
-// */
-
-// //Code Here
-
-
-
-// /*
-//   The activity we just did is very much how data works in 'the real world'.
-// */
+/*
+  The activity we just did is very much how data works in 'the real world'.
+*/
